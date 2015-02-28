a2268dcb5ce44267a0a30fe90d063e26de9073c9
emoonh <emoon@massart.edu>
2015-02-28 01:38:35 -0500

    fixed assignment 3 css & scss and continued on js form validation

--- 05 assignment/js/form.js
+++ 05 assignment/js/form.js
@@ -7,15 +7,21 @@ document.getElementById("submit").onclick = function() {
 	if (ln.value.length <= 2){
 		ln.classList.add("error");
 	}
+	/*
 	var ea = document.getElementsByName("ea")[0];
-	if (ea.contains.)
-	
+	if (ea.value.test(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+		ea.classList.add("error");
+	}
+	*/
 	var un = document.getElementsByName("un")[0];
 	if (un.value.length <= 7){
 		un.classList.add("error");
 	}		
+	var pw = document.getElementsByName("pw")[0];
+	if (pw.value.length <= 8 || !pw.value.match(/\d+/)){
+		pw.classList.add("error");
+	}		
 	return false;
-	
 };
 
 
